5a383fb9b9bad3692cdc16be15aaf052edf14e2f
Trey Arnold <arnold.trey80@gmail.com>
2019-04-30 14:49:24 -0500

    all 3 api keys now importing

--- keys.js
+++ keys.js
@@ -1,6 +1,20 @@
 console.log('this is loaded');
 
-exports.spotify = {
+spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+ticketMaster = {
+  id: process.env.TM_ID,
+  secret: process.env.TM_SECRET
+};
+
+omdb = {
+  id: process.env.OMDB_ID
+};
+
+module.exports = {
+  spotify, 
+  ticketMaster, 
+  omdb};
--- liri.js
+++ liri.js
@@ -2,10 +2,23 @@ require("dotenv").config();
 
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
+const ticketMaster = new TicketMaster(keys.ticketMaster);
+const omdb = new OMDB(keys.omdb);
 
 function Spotify(keysObject) {
   this.id = keysObject.id;
   this.secret = keysObject.secret;
 }
 
+function TicketMaster(keysObject) {
+  this.id = keysObject.id;
+  this.secret = keysObject.secret;
+}
+
+function OMDB(keysObject) {
+  this.id = keysObject.id;
+}
+
 console.log(spotify);
+console.log(ticketMaster);
+console.log(omdb);
