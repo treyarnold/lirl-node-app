d25565bdc2e2d2615dc43d615644a6edfc10d77d
Trey Arnold <arnold.trey80@gmail.com>
2019-04-30 14:18:20 -0500

    Got the spotify api to be passed in from env

--- liri.js
+++ liri.js
@@ -3,4 +3,9 @@ require("dotenv").config();
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 
+function Spotify(keysObject) {
+  this.id = keysObject.id;
+  this.secret = keysObject.secret;
+}
+
 console.log(spotify);
