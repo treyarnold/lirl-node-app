7b31f3f0b3cfbbf5de638da4a3113868fd97e71f
treyarnold <arnold.trey80@gmail.com>
2019-05-02 18:35:33 -0500

    3 main choices done

--- concertSearch.js
+++ concertSearch.js
@@ -9,6 +9,17 @@ function TicketMaster(keysObject) {
   this.secret = keysObject.secret;
 }
 
+function formatDate(date) {
+  let dateNumbers = date.split('-');
+  let newDate = `${dateNumbers[1]}/${dateNumbers[2]}/${dateNumbers[0]}`
+  return newDate
+}
+
+function displayConcert(concert) {
+  console.log(`\n${concert._embedded.events[0].name}`);
+  console.log(`Venue: ${concert._embedded.events[0]._embedded.venues[0].name}`);
+  console.log(`Date: ${formatDate(concert._embedded.events[0].dates.start.localDate)}`);
+}
 
 module.exports = function () {
   console.log("in concert");
@@ -17,9 +28,9 @@ module.exports = function () {
     message: "What artist would you like to see in concert?",
     name: "artist"
   }]).then(concert => {
-    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMaster.id}?keyword=${concert.artist}`).then(
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMaster.id}&keyword=${concert.artist}`).then(
       response => {
-        console.log(response.data);
+        displayConcert(response.data);
       }
     );
   })
