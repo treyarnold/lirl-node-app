1f77f8438d7e6e8a70fc1455ff0abd19c9ecb251
treyarnold <arnold.trey80@gmail.com>
2019-05-02 14:29:10 -0500

    movie function done

--- movieSearch.js
+++ movieSearch.js
@@ -8,6 +8,36 @@ function OMDB(keysObject) {
   this.id = keysObject.id;
 }
 
+// Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+
+function displayMovie(movieData) {
+  console.log(movieData.Title);
+  console.log(`Year: ${movieData.Year}`);
+  console.log(`Rotten Tomatoes: ${movieData.Ratings[1].Value}`);
+  console.log(`Produced in ${movieData.Country}`);
+  console.log(`Language: ${movieData.Language}`);
+  console.log(`Plot: ${movieData.Plot}`);
+  console.log(`Actors: ${movieData.Actors}`);
+}
+
 module.exports = function () {
-  console.log("in movie Search");
+  inquirer.prompt([{
+    type: "input",
+    message: "What movie would you like to search for?",
+    name: "title"
+  }]).then(movie =>{
+    if (movie.title.includes("")) movie.title = movie.title.replace(/ /g, "+")
+    axios.get(`http://www.omdbapi.com/?t=${movie.title}&y=&plot=short&apikey=${omdb.id}`).then(
+      function(response) {
+        displayMovie(response.data);
+      }
+    );
+  })
 }
